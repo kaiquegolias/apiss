a48925c5e64cc0c97e3d177c1217449ac65bc712
kaiquegolias <kaiquemnm@gmail.com>
2025-03-28 12:33:35 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,4 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
 
+// Exportação nomeada (sem default)
 export const supabase = createClient(supabaseUrl, supabaseKey)
